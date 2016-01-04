27fcca9f6f8df62892b39bb4f8d6b461c0c277ea
Mathijs <r.j.m.degroot@student.utwente.nl>
2016-01-04 12:00:09 +0100

    init

---
+++ controller/router.js
@@ -0,0 +1,30 @@
+/**
+ * Created by Mathijs on 2-1-2016.
+ */
+//modules
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res) {
+    res.render('index');
+});
+router.get('/news', function(req, res) {
+    res.render('news');
+});
+router.get('/available', function(req, res) {
+    res.render('available');
+});
+router.get('/jobsinarea', function(req, res) {
+    res.render('jobsinarea');
+});
+router.get('/jobs', function(req, res) {
+    res.render('jobs');
+});
+router.get('/myProfile', function(req, res) {
+    res.render('myProfile');
+});
+router.get('/register', function(req, res) {
+    res.render('register');
+});
+
+module.exports = router;
